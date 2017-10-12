c92ab2813c68e22e036fa132ccd33d423f15928e
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2017-10-12 20:10:15 +0100

    Initialize rspec directory and config files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,13 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 
 # require database cleaner at the top level
 require 'database_cleaner'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # [...]
 # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
@@ -16,6 +23,8 @@ RSpec.configuration do |config|
   # [...]
   # add `FactoryGirl` methods
   config.include FactoryGirl::Syntax::Methods
+
+  config.include RequestSpecHelper, type: :request
 
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
